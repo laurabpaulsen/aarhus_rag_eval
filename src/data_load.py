96251f54d43d2582d67477e6a9fe4bfae011fbc2
Malte Lau Petersen (UCloud) <maltelau@mlpetersen.dk>
2023-12-08 11:42:32 +0100

    logging and html2text

--- src/data_load.py
+++ src/data_load.py
@@ -4,11 +4,14 @@ import json
 from pathlib import Path
 # from llama_index import download_loader
 from langchain.document_loaders import JSONLoader
-from langchain.schema.document import Document, BaseDocumentTransformer
-from typing import Any, Sequence
+from langchain.document_transformers import Html2TextTransformer
+
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
-root_path = Path.cwd().parent
+root_path = Path.cwd()
+if root_path.name == "src": root_path = root_path.parent
 default_path = root_path / "data" / "loop_q_and_a_w_meta.json"
 docs_path = root_path / "data" / "loop_documents_w_meta.json"
 
@@ -39,40 +42,35 @@ def metadata_loop_document(record: dict, metadata: dict) -> dict:
 
 
 def load_documents(doc_path: Path = docs_path):
-    # JSONReader = download_loader("JSONReader")
+    logging.info("Loading documents: start")
     loader = JSONLoader(doc_path,
                         jq_schema=".[] | select(.document_body != null)",
                         content_key="document_body",
                         metadata_func = metadata_loop_document)
     documents = loader.load()
-    return documents
+    logging.info("Loading documents: finished")
+    logging.info("Remove html: start")
+    html2text = Html2TextTransformer()
+    docs_plaintext = html2text.transform_documents(documents)
+    logging.info("Remove html: finished")
+    return docs_plaintext
 
 
-class PlainTextTransformer(BaseDocumentTransformer):
-    """
-    Turn a document's page_content into plaintext by removing html tags
-    see https://github.com/langchain-ai/langchain/discussions/7497
-    """
-    def transform_documents(self, documents: Sequence[Document], **kwargs: Any) -> Sequence[Document]:
-        for document in documents:
-            document.page_content = document.page_content
-
-    async def atransform_documents(
-        self, documents: Sequence[Document], **kwargs: Any
-    ) -> Sequence[Document]:
-        # Implement the asynchronous version of the method
-        return self.transform_documents(documents, **kwargs)
 
 
 if __name__ == '__main__':
+    logging.debug("Running tests for data_load")
     jsondata1 = load_loop()
+    assert len(jsondata1) == 700
+
     jsondata2 = load_loop_jsonl(root_path / "data" / "loop_q_and_a_w_ref_text_meta.jsonl")
+    logging.debug(f"Found {len(jsondata2)} answers")
 
     ## investigate entries with internal references
     questions = [doc['question'] for doc in jsondata2 if 'internal_reference_texts' in doc.keys()]
     responses = [doc['response'] for doc in jsondata2 if 'internal_reference_texts' in doc.keys()]
-
     documents = [doc['internal_reference_texts'] for doc in jsondata2 if 'internal_reference_texts' in doc.keys()]
+    logging.debug(f"Found {len(questions)} answers with internal_reference")
 
     documents2 = load_documents()
-    assert len(jsondata1) == 700
+    logging.debug(f"Found {len(documents2)} internal documents")
