15d94d5e01af569f2a64aa30bfee923016ed00fc
laurabpaulsen <202005791@post.au.dk>
2023-12-14 13:53:57 +0100

    loop over types of generation and save results to dict

--- src/eval_NER_overlap.py
+++ src/eval_NER_overlap.py
@@ -6,63 +6,66 @@ import dacy
 
 if __name__ in "__main__":
     
-    jsondata = load_loop()[:5]
+    jsondata = load_loop()
     root_dir = Path(__file__).parents[1]
+
+    results_path = root_dir / "results"
+    results_path.mkdir(parents=True, exist_ok=True)
+
+    generated_path = root_dir / "data" / "generated"
 
     nlp = dacy.load("medium")
 
     # add ner to pipeline
     nlp.add_pipe("dacy/ner-fine-grained", config={"size": "medium"})
 
+    # get all the generated answers
+    files = [f for f in generated_path.iterdir() if f.suffix == ".json"]
 
-    # load generated answers
-    generated_path = root_dir / "data" / "generated" / "mistral_rag.json"
-    with open(generated_path, "r") as f:
-        generated_answers = json.load(f)
-
-    # only keep the answers
-    generated_answers = [answer["answer"] for answer in generated_answers]
 
     loop_answers = [answer for answer in map_filter(jsondata, "response") if answer is not None]
 
-    overlap = []
+    results = {}
+    for gen_file in files:
+        with open(generated_path / gen_file, "r") as f:
+            generated_answers = json.load(f)
 
-    for i, (answer_loop, generated_answer_gen) in enumerate(zip(loop_answers, generated_answers)):
-
-        # get the entities
-        loop_nlp = nlp(answer_loop)
-        gen_nlp = nlp(generated_answer_gen)
-
-        loop_entities = [ent.text for ent in loop_nlp.ents]
-        gen_entities = [ent.text for ent in gen_nlp.ents]
-
-        # compare the entities
-        overlapping_ents = [entity for entity in loop_entities if entity in gen_entities]
+        overlap = []
         
-        # total number of entities
-        total_ents = len(loop_entities) + len(gen_entities)
-
-        # percentage of overlapping entities
-        overlap.append(len(overlapping_ents) / total_ents)
-
-        print(f"Loop: {loop_entities}")
-        print(f"Generated: {gen_entities}")
-
-        # parts of speech tags
-        loop_pos = [token.pos_ for token in loop_nlp]
-        gen_pos = [token.pos_ for token in gen_nlp]
-        print ("------------------")
-        #print(f"Loop: {loop_pos}")
-        #print(f"Generated: {gen_pos}")
-        #print ("------------------")
-
-
-    print(f"Average overlap: {sum(overlap) / len(overlap)}")
-
-
-
-
-
         
+        generated_answers = [answer["answer"] for answer in generated_answers]
+        for i, (answer_loop, answer_gen) in enumerate(zip(loop_answers, generated_answers)):
+
+            # get the entities
+            loop_nlp = nlp(answer_loop)
+            gen_nlp = nlp(answer_gen)
+
+            loop_entities = [ent.text for ent in loop_nlp.ents]
+            gen_entities = [ent.text for ent in gen_nlp.ents]
+
+            # compare the entities
+            overlapping_ents = [entity for entity in loop_entities if entity in gen_entities]
+            
+            # total number of entities
+            total_ents = len(loop_entities) + len(gen_entities)
+
+            # percentage of overlapping entities
+            try:
+                overlap.append(len(overlapping_ents) / total_ents)
+            except ZeroDivisionError:
+                overlap.append(0)
+
+
+        results[gen_file.stem] = {"ner_overlap": sum(overlap) / len(overlap)}
+    
+    # save to json
+    with open(results_path / "ner_overlap.json", "w", encoding="utf-8") as json_file:
+        json.dump(results, json_file, ensure_ascii=False, indent=4)
+
+
+
+
+
+            
 
 
