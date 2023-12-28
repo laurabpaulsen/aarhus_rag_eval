29a7df48177c4b502a40e3ae43eae8924b387852
Malte Lau Petersen <maltelau@mlpetersen.dk>
2023-12-28 16:47:29 +0100

    spellchecking + fast tokenization

--- src/eval_readability.py
+++ src/eval_readability.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 
-import logging
 from pathlib import Path
 import dacy
 from rouge_score import tokenizers
 from tqdm import tqdm
 import jsonlines
 import json
+import aspell
 
-
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 class DacyTokenizer(tokenizers.Tokenizer):
     def __init__(self):
         self.nlp = dacy.load("large")
 
     def tokenize(self, text):
-        return [x for x in self.nlp(text)]
+        return [x for x in self.nlp.tokenizer(text)]
 
 
-def lix(text, tokenizer = DacyTokenizer()):
-    if text.strip()[-1] not in ".!?":
+def add_ending_punctuation(text: str, punct: str = ".!?") -> str:
+    if text.strip()[-1] not in punct:
         text += "."
+    return text
+
+def tokenize(text: str, tokenizer = DacyTokenizer()) -> list[str]:
     tokens = tokenizer.tokenize(text)
+    return tokens
+
+
+def lix(text, tokens):
     n_sentences = text.count(".") + text.count("?") + text.count("!")
     n_tokens = len(tokens)
     n_long_words = len([x for x in tokens if len(x) > 6])
@@ -30,10 +38,10 @@ def lix(text, tokenizer = DacyTokenizer()):
     return lix
 
 
-# def spellcheck(text):
-#     spellcheck_results = None
-#     n_spelling_errors = len(spellcheck_results)
-#     return n_spelling_errors
+def spellcheck(tokens, spellchecker = aspell.Speller(("lang", "da"), ("run-together", "true"))):
+    spellcheck_results = [spellchecker.check(str(t)) for t in tokens if t] # dont test empty strings
+    n_spelling_errors = len([t for t in spellcheck_results if not t]) # how many are true
+    return n_spelling_errors / len(spellcheck_results)
 
 
 if __name__ in "__main__":
@@ -48,20 +56,30 @@ if __name__ in "__main__":
     # all files with generated answers
     files = [f for f in generated_path.iterdir() if f.suffix == ".jsonl"]
 
-
-
     results = {}
 
     for gen_file in files:
-        logging.info(f"Calculating LIX for {str(gen_file)}")
         with jsonlines.open(gen_file) as f:
+            logging.info(f"Tokenizing answers from {str(gen_file)}")
             # all generated answers
-            generated_answers = [answer["answer"] for answer in f]
+            generated_answers = [add_ending_punctuation(answer["answer"]) for answer in f]
+            tokens = [tokenize(text) for text in tqdm(generated_answers)]
+
+
+        logging.info(f"Calculating LIX for {str(gen_file)}")
 
         lix_results = {
-            "lix_answer": [lix(text) for text in tqdm(generated_answers)]
+            "lix_answer": [lix(text,token) for text,token in tqdm(zip(generated_answers, tokens))]
             }
         results[gen_file.stem] = lix_results
+
+
+        logging.info(f"Counting spelling mistakes for {str(gen_file)}")
+
+        spellcheck_results = {
+            "spellcheck_answer": [spellcheck(token) for token in tqdm(tokens)]
+            }
+        results[gen_file.stem].update(spellcheck_results)
 
 
     # save to json
