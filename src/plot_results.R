6da4b9e5964d9c65571feff45340ed06c517ec4c
Malte Lau Petersen <maltelau@mlpetersen.dk>
2023-12-31 09:22:37 +0100

    updated plot

--- src/plot_results.R
+++ src/plot_results.R
@@ -18,8 +18,8 @@ calc_ner_f1 <- function(.data, ...) {
 d <- read_csv("results/merged_eval_results.csv") %>%
   # rescale to 0-10 scale
   #sample_n(10) %>%
-  mutate(lix_response = lix_response / max(lix_response) * 10) %>%
-  mutate(spellcheck_response = spellcheck_response * 10) %>%
+  #mutate(lix_response = lix_response / max(lix_response) * 10) %>%
+  mutate(spellcheck_response = spellcheck_response * 100)
   calc_ner_f1("correctness_response", "informativeness_gold",
               "informativeness_response", "knowledge_response",
               "faithfulness_response")
@@ -31,7 +31,11 @@ llm_eval_scores <- d %>%
          -starts_with("correctness_"), -starts_with("knowledge")) %>%
   pivot_longer(!c(model, id)) %>%
   #filter(name == "correctness") %>%
-  filter(!is.na(value))
+  filter(!is.na(value)) %>%
+  mutate(is_llm = ifelse(str_detect(name, "response"),
+                         "Automatic Readability",
+                         "LLM Eval")) %>%
+  mutate(name = str_split(name, "_") %>% map(pluck, 1) %>% unlist(9))
 
 
 countbased <- d %>%
@@ -48,32 +52,44 @@ countbased <- d %>%
 
 
 ## histogram of
-## llm eval scores
-llm_eval_scores %>%
-  #group_by(model) %>%
-  #filter(id < 10) %>%
-  #summarise(across(!id, \(x) mean_se(na.exclude(x)))) %>%
-  #select(-document) %>%
-  #filter(model != "mixtral-simple-prompt") %>%
-  #filter(name == "correctness") %>%
-  ggplot() +
-  #geom_line(aes(factor(name), y=value, color=model, group=model), stat="summary", fun.y=mean) #+
-  #geom_density_ridges(aes(x = value, y = factor(name), group = str_c(name, model), color = model), fill = NA, stat="binline", bins = 11, scale = 0.95)
-  geom_histogram(aes(x=value, fill=model, group = str_c(name, model)), color=NA, position=position_dodge(width=0.7), bins=11) +
-  #stat_count(aes(x = value, fill=model)) +
-  #geom_line(aes(x = factor(model), y=value, group=id), alpha=.15) + #+
-  #geom_pointrange(stat="summary", fun.data=mean_cl_normal, alpha=0.2, color=NA)+
-  #coord_cartesian(ylim = c(0,10))  +
-  scale_x_continuous(breaks = seq(0,10)) +
-  facet_grid(name ~. )
+## ## llm eval scores
+## llm_eval_scores %>%
+##   #group_by(model) %>%
+##   #filter(id < 10) %>%
+##   #summarise(across(!id, \(x) mean_se(na.exclude(x)))) %>%
+##   #select(-document) %>%
+##   #filter(model != "mixtral-simple-prompt") %>%
+##   #filter(name == "correctness") %>%
+##   ggplot() +
+##   #geom_line(aes(factor(name), y=value, color=model, group=model), stat="summary", fun.y=mean) #+
+##   #geom_density_ridges(aes(x = value, y = factor(name), group = str_c(name, model), color = model), fill = NA, stat="binline", bins = 11, scale = 0.95)
+##   geom_histogram(aes(x=value, fill=model, group = str_c(name, model)), color=NA, position=position_dodge(width=0.7), bins=11) +
+##   #stat_count(aes(x = value, fill=model)) +
+##   #geom_line(aes(x = factor(model), y=value, group=id), alpha=.15) + #+
+##   #geom_pointrange(stat="summary", fun.data=mean_cl_normal, alpha=0.2, color=NA)+
+##   #coord_cartesian(ylim = c(0,10))  +
+##   #scale_x_continuous(breaks = c(seq(0,10), 20, 40, 60, 80)) +
+##   facet_grid(name ~. )
 
 ## ecdf
 llm_eval_scores %>%
   ggplot() +
-  stat_ecdf(aes(value, color = model)) +
-  scale_x_continuous(breaks = seq(0,10)) +
-  facet_grid(name~.)
+  stat_ecdf(aes(value, color = model, linetype = fct_rev(is_llm))) +
+  #scale_x_continuous(breaks = c(0.5, seq(0,10))) +
+  facet_wrap(~name, scales="free") +
+  theme_bw() +
+  scale_color_viridis_d(begin=1/3) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "bottom",
+        legend.box = "vertical",
+        panel.grid.minor.x = element_blank(),
+        text = element_text(size = 16),
+        axis.text = element_text(size = 10)) +
+  labs(fill = "Source", color = "Source", linetype = "Type",
+       x = "", y = "")
 
+ggsave("plots/results_lexical_semantic.png")
 
 ## ## histogram of
 ## ## eval scores
